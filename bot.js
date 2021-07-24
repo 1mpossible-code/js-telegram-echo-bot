b907d382db71dfccdc146a8203fc68a69c8c462e
Max <linme00p@gmail.com>
2021-07-24 17:59:00 +0300

    Create echo logic in bot.js

---
+++ bot.js
@@ -0,0 +1,27 @@
+import {Telegraf} from 'telegraf'
+import dotenv from "dotenv";
+
+// Configure 'dotenv'
+dotenv.config();
+
+// Create bot instance
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Bot 'start' command handler
+bot.command('start', (ctx) => {
+    // Send message
+    ctx.reply('Hello!');
+})
+
+// Echo handler
+bot.on('text', ctx => {
+    // Reply message text
+    ctx.reply(ctx.message.text);
+})
+
+// Launch bot
+await bot.launch();
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
